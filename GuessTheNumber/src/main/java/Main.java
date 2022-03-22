1b2587c4ebf451831bdf837c2aa99a976f3c9864
Jatin Patel <jatinjay10@gmail.com>
2022-03-22 14:43:51 -0400

    fixed escape character and infinite loop

--- GuessTheNumber/src/main/java/Main.java
+++ GuessTheNumber/src/main/java/Main.java
@@ -15,14 +15,16 @@ public class Main {
         //game and player introduction
         System.out.println("Hello! What is your name?");
         name = scan.nextLine();
-        System.out.printf(String.format("Well, %s, I am thinking of a number between 1 and 20./nTake a guess.",name));
+        System.out.printf(String.format("Well, %s, I am thinking of a number between 1 and 20.\nTake a guess.",name));
 
 
         //reset point
         while(input.equals("y")){
             //game loop
             while(guess!=answer){
+
                 //make sure user gives valid input
+                inputAccepted=false;
                 while (!inputAccepted) {
                     try {
                         guess = Integer.valueOf(scan.nextLine());
@@ -34,9 +36,9 @@ public class Main {
                 if(guess==answer)
                     break;
                 else if (guess < answer)
-                    System.out.println("Your guess is too low./nTake a guess.");
+                    System.out.println("Your guess is too low.\nTake a guess.");
                 else if (guess > answer)
-                    System.out.println("Your guess is too high./nTake a guess.");
+                    System.out.println("Your guess is too high.\nTake a guess.");
             }
             //player guessed right
             System.out.println(String.format("Good job, %s! You guessed my number in %i guesses!",name,count));
