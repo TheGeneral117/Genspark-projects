8ec2f7a9e2fdc6db36a13334094d457ba9dfb11c
Jatin Patel <jatinjay10@gmail.com>
2022-03-22 13:45:48 -0400

    added and fixed user input check

--- DragonCave/src/main/java/Main.java
+++ DragonCave/src/main/java/Main.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //get random room
+        //variable declaration
+        int choice=0;
         Random rand = new Random();
+        boolean inputAccepted = false;
+
+        //get random room
         int room = rand.nextInt(2)+1;
         //create input object
         Scanner scanner = new Scanner(System.in);
@@ -13,17 +17,22 @@ public class Main {
         System.out.println("you see two caves. In one cave, the dragon is friendly");
         System.out.println("and will share his treasure with you.  The other dragon");
         System.out.println("is greedy and hungry and will eat you on sight.");
-        System.out.println("Which cave will you go into? (1 or 2)");
-        // user input
-        int input = scanner.nextInt();
+
         //make sure user gives valid input
-        while(input != 1 || input != 2){
-            System.out.println("There is no other cave, please choose 1 or 2");
-            input = scanner.nextInt();
+        while (!inputAccepted) {
+            try {
+                System.out.println("Which cave will you go into? (1 or 2)");
+                choice = Integer.valueOf(scanner.nextLine());
+                if(choice == 1 || choice == 2)
+                    inputAccepted = true;
+            } catch (NumberFormatException e) {
+
+            }
         }
 
-        //
-        if(input==room){
+
+        //result of choice
+        if(choice==room){
             System.out.println("You approach the cave...");
             System.out.println("It is dark and spooky...");
             System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
@@ -35,6 +44,6 @@ public class Main {
             System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
             System.out.println("says 'come share my treasure!'");
         }
-        
+
     }
 }
